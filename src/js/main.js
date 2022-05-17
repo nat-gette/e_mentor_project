6245a1b3b26423061c8f6fb5e49cac6b8dbb6a01
nat-gette <natalagette@gmail.com>
2022-05-17 18:30:10 +0500

    burger btn and menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,24 @@
-const signInForm = document.querySelector('.sign-in__form');
-const signInLabels = document.querySelectorAll('.form-label');
-const signInInputs = document.querySelectorAll('.sign-in__input');
-const signInEmail = document.querySelector('.sign-in__input--email');
-const signInPass = document.querySelector('.sign-in__input--pass');
-const signInError = document.querySelectorAll('.sign-in__error');
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
 
+// myModal.addEventListener('shown.bs.modal', function () {
+//     myInput.focus()
+// })
 
-signInForm.onsubmit = function () {
-    let emailVal = signInEmail.value;
-    let emailPass = signInEmail.value;
-    let emptyInputs = Array.from(signInInputs).filter(input => input.value === '');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
 
-    signInInputs.forEach( function (input) {
-        if (input.value === '') {
-            input.classList.add('sign-in__input--error');
-            console.log('err');
-        } else {
-            input.classList.remove('sign-in__input--error');
+let menuOpen = false;
 
-        }
-    });
-
-    if (signInEmail.value === '') {
-        signInError[0].classList.add('sign-in__error-active');
-        signInLabels[0].classList.add('sign-in_input-label--error')
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('menu-open');
+        menu.classList.add('menu-active');
+        menuOpen = true;
     } else {
-        signInError[0].classList.remove('sign-in__error-active');
-        signInLabels[0].classList.remove('sign-in_input-label--error')
+        menuBtn.classList.remove('menu-open');
+        menu.classList.remove('menu-active');
+        menuOpen = false;
+    }
+});
 
-    };
-
-    if (signInPass.value === '') {
-        signInError[1].classList.add('sign-in__error-active');
-        signInLabels[1].classList.add('sign-in_input-label--error')
-    } else {
-        signInError[1].classList.remove('sign-in__error-active');
-        signInLabels[1].classList.remove('sign-in_input-label--error')
-
-    };
-
-    if (emptyInputs.length !== 0) {
-        console.log('not field');
-        return false;
-    };
-
-
-}
-
-signInEmail.onclick = function () {
-    signInError[0].classList.remove('sign-in__error-active');
-}
-
-signInPass.onclick = function () {
-    signInError[1].classList.remove('sign-in__error-active');
-}
